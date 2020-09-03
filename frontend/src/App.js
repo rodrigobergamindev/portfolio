88dcf12a9d923d249ef4e8753d4e81b6b113c20d
Rodrigo <rodrigobergamindev@gmail.com>
2020-09-03 13:38:14 -0300

    finalizaçao card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import {Container} from './style/index';
 import Header from './components/Header.jsx';
 import './index.css'
-import Navbar from './components/Navbar';
-import Techs from './components/Techs';
 
 
 
 function App() {
   return (
-    <Container>
-      <Navbar/>
-      <Techs/>
-    </Container>
+      <Container>
+        <Header/>
+      </Container>
+
   );
 }
 
--- frontend/src/style/index.js
+++ frontend/src/style/index.js
@@ -2,155 +2,88 @@ import styled from 'styled-components';
 
 
 const Container = styled.div`
-    color: #f5f6fa;
-    display:flex;
-    flex-direction:column;
-`;
-
-
-
-
-/*HEADER*/
-
-const ContainerHeader = styled(Container)`
-   header {
-       display:flex;
-       flex-direction: column;
-        align-items: center;
-        justify-content: center;
-
-   }
-   img {
-       border-radius: 50%;
-       margin-top: 4em;
-       max-weight: 18em;
-       max-width: 18em;
-       transition: 1s;
-       border: 0.5em solid #40407a;
-
-       @media only screen and (max-width: 600px) {
-        max-weight: 14em;
-        max-width: 14em;
-      }
-
-      @media only screen and (max-width: 370px) {
-        max-weight: 12em;
-        max-width: 12em;
-      }
-
-       &:hover {
-        transition: all .5s ease-in-out;
-        transform: scale(1.1);
-       }
-   }
-
-   
-   h1{
-    margin-top: 0.9em;
-    text-align: center;
-    font-size: 5em;
-    font-weight: 400;
-
-    @media only screen and (max-width: 600px) {
-        font-size: 3em;
-      }
-
-      @media only screen and (max-width: 370px) {
-        font-size: 2em;
-      }
-
-}
-
-    h3 {
-        font-weight: 100;
-        font-size: 2.5em;
-        margin-top: 0.5em;
-        text-align: center;
-
-        @media only screen and (max-width: 600px) {
-            font-size: 1.4em;
-          }
-    
-          @media only screen and (max-width: 350px) {
-            font-size: 1.2em;
-          }
-    }
-`
-
-/**NAVBAR */
-
-const ContainerNav = styled(Container)`
-    font-weight: 400;
-    background-color: #353b48;
-    flex-direction: row;
-    list-style-type:none;
-    justify-content: center;
-    ul {
-        background-color: #353b48;
-        display:flex;
-        list-style-type:none;
-        justify-content: space-around;
-        width: 35em;
-        flex-wrap: wrap;
-
-        
-        li {
-            padding: 0.8em;
-            background-color: #353b48;
-            &:hover, active {
-               color:#40407a;
-            }
-        }
-    }
-
-`
-
-
-const ContainerCards = styled(Container)`
-    flex-direction: row;
-    justify-content: space-around;
-    flex-wrap: wrap;
-    align-items:center;
-
-
-`
-
-const ContainerTech = styled(Container) `
-    justify-content: space-around;
-    margin-top: 3em;
-    h1 {
-      font-weight: 400;
-      font-size: 2.7em;
-      text-align: center;
-      color: white;
-      margin-bottom: 2em;
-    }
+display:flex;
+align-items: center;
+justify-content: center;
+flex-wrap: wrap;
 `
 
 const Card = styled.div`
+  box-sizing: border-box;
+  margin-top: 12em;
+  max-width: 450px;
+  color: #718093;
+  padding: 3em;
 
-    max-width: 250px;
-    height: 410px;
-    border-radius:4%;
-    background-color:#f5f6fa;
-    color: #2d3436;
-    padding: 0.4em;
-    word-wrap: break-word;
+  @media only screen and (max-width: 600px) {
+   max-width: 300px;
+  }
 
-    display: flex;
-    flex-direction: column;
 
-    hr {
-      top: 0;
-    }
-    img {
-      margin-left: .8em;
-      max-width: 50px;
-      border-radius: 50%;
-    }
+  box-shadow: 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22);
+
+  background-color:#dcdde1;
+  border-radius: 1%;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+
+h1{
+  font-weight: 100;
+  letter-spacing: 2.7px;
+  margin-top: 0.6em;
+  text-align: center;
+  font-size: 1.6em;
+  font-weight: 400;
+
+  @media only screen and (max-width: 600px) {
+    font-size: 1.5em;
+   }
+
+}
+
+
+  h3 {
+      font-weight: 300;
+      font-size: 0.9em;
+      margin-top: 0.5em;
+      text-align: center;
+
+      @media only screen and (max-width: 600px) {
+        font-size: 0.9em;
+       }
+  }
+
+
+img {
+  border-radius: 50%;
+  max-width: 9.1em;
+  margin-bottom: 0.9em;
+  z-index: 100;
+}
+
+  ul {
+    margin-top: 1.2em;
+    width: 18em;
+      display:flex;
+     align-items: center;
+     justify-content: center;
+     list-style-type:none;
+
+     li {
+       margin: 0.8em;
+       transition: 1s;
+
+       &:hover {
+        transition: all .5s ease-in-out;
+        transform: scale(1.4);
+       }
+     }
+  }
+
 `
 
 
-
-
-export {Container, ContainerHeader, ContainerNav, ContainerCards, ContainerTech, Card}
+export {Container, Card}
